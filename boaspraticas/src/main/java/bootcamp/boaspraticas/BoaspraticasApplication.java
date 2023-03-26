f255e180e1207e3a6600b0343eed6569143c3527
gabileitao <marialeitaoautora@gmail.com>
2023-03-26 00:00:28 -0300

    projeto de padrãos de projeto em JAVA

---
+++ boaspraticas/src/main/java/bootcamp/boaspraticas/BoaspraticasApplication.java
@@ -0,0 +1,13 @@
+package bootcamp.boaspraticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoaspraticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoaspraticasApplication.class, args);
+	}
+
+}
---
+++ boaspraticas/src/test/java/bootcamp/boaspraticas/BoaspraticasApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.boaspraticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoaspraticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
